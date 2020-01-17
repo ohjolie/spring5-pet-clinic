3a0c65032892a2507a3599bb833df7a551454528
Jolie <zhuli0413@gmail.com>
2020-01-17 15:45:34 -0500

    Initialize Project Add.

---
+++ src/test/java/jolie/springframework/spring5petclinic/Spring5PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package jolie.springframework.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
